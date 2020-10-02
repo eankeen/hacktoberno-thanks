f2afa8dea43948246ec46dd549dad49ca1696fe1
Edwin Kofler <24364012+eankeen@users.noreply.github.com>
2020-10-02 09:49:58 -0700

    add create branch

---
+++ create-branch.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -eu
+
+git checkout -b "$1"
+echo "awesome thing" > test.txt
+git commit --no-edit -m "" --allow-empty --allow-empty-message
+gh pr create --title "$1" --body ""
+git switch main
